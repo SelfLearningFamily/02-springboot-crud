1be0725eba599135f9a3272095848135e8280e23
msiddiqui <msiddiqui@i2cinc.com>
2023-11-28 01:54:33 +0500

    Springboot CRUD DB Files
    adding the sql scripts that I used during the development / learning

---
+++ src/main/java/com/zbrickx/db/currentRecords.sql
@@ -0,0 +1,8 @@
+
+use student_tracker;
+ALTER TABLE `student` AUTO_INCREMENT = 1;
+INSERT INTO `student` (`first_name`,`last_name`,`email`) VALUES ('Hercules','Duck','Hercules.duck@gmail.com');
+INSERT INTO `student` (`first_name`,`last_name`,`email`) VALUES ('jhon','Doe','scobbydobby@do.com');
+INSERT INTO `student` (`first_name`,`last_name`,`email`) VALUES ('Scobby','Doe','scobbydobby@do.com');
+INSERT INTO `student` (`first_name`,`last_name`,`email`) VALUES ('zaki','siddiqui','zaki.siddiqui@gmail.com');
+INSERT INTO `student` (`first_name`,`last_name`,`email`) VALUES ('Scobby','Duck','daffy.duck@gmail.com');
